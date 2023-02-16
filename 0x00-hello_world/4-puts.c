60b7b77a914c305a4a68f96159d38a3e0580d716
perismuniu <peris.muniu@gmail.com>
2023-02-16 14:34:22 -0800

    100-intel

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - A program that prints a line using puts function in c programming
+ *return: 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
