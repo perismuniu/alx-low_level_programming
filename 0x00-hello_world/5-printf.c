6bccd159bd6225b608ee4e0bfa595161edf3f21f
perismuniu <peris.muniu@gmail.com>
2023-02-16 16:06:36 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * main -A program that prints using the printf function in c programming.
- * Return 0 (Success)
+ * main - a program that uses printf function to print a line
+ * Return: 0 (Success)
  */
-int main(void);
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
-}
+fput(
