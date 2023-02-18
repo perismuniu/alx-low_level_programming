9c1f32b21947f7ad519fcb1723beb8c250e06ed6
perismuniu <peris.muniu@gmail.com>
2023-02-17 16:15:39 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - program prints all possible different combinations of three digits
+ * main - program printing all possible combinations of three digits
  * Return: 0
  */
 int main(void)
@@ -11,22 +11,22 @@ int main(void)
 	{
 		j = i / 100; /* hundreds */
 		k = (i / 10) % 10; /* tens */
-		l = i % 10; /* singles*/
+		l = i % 10; /* singles */
 
 		if (j < k && k < 1)
 		{
 			putchar(j + '0');
 			putchar(k + '0');
-		        putchar(l + '0');
-			
-			if (i < 700
+			putchar(l + '0');
+
+			if (i < 700)
 			{
 				putchar(44);
 				putchar(32);
 			}
 		}
 	}
-	putchar('\n');
+	putchar('\n);
 
 	return (0);
 }
