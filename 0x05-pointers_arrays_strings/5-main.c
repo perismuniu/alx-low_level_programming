2bf9f9a6983fdbbd2c624b5cff0af1d43cb24323
perismuniu <peris.muniu@gmail.com>
2023-02-27 04:33:47 -0800

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+julien@ubuntu:~/0x05$ cat 5-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
+}
