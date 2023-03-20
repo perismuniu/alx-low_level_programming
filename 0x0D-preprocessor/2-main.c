ade4179b1294fbaa7c463a79eaff44495703a575
perismuniu <peris.muniu@gmail.com>
2023-03-20 06:47:15 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 #define NAME __FILE__
+
 /**
- * main -Entry point
- * program that prints name of file compile followed by new line.
- * Return: Always 0.
+ * main - Prints name of file
+ * Return: Always 0(success)
  */
 
 int main(void)
 {
-	printf("File name: %s\n", NAME);
+	printf("%s\n", NAME);
 	return (0);
 }
