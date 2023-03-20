8a10d7968fc2e965819df0f367826a252d73adc4
perismuniu <peris.muniu@gmail.com>
2023-03-20 06:31:11 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * program that prints name of file compile followed by new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("File name: %s\n", __FILE__);
+	return (0);
+}
