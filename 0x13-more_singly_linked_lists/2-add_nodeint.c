329bfd08ee0389c49fbc4d61b279d64b93ade25c
perismuniu <peris.muniu@gmail.com>
2023-04-04 14:27:37 -0700

    ammendment -   adds a new node at the beginning of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * add_nodeint - add  a new node at the beginning.
- * @*head: head for struct listint_t.
+ * @head: head for struct listint_t.
  * @n: node1.
  * *Return: Address of new element or NULL if failed.
  */
@@ -11,17 +11,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node1;
 
-	node1 = (listint_t *)malloc(sizeof(listint_t));
+	node1 = malloc(sizeof(listint_t));
 
 	if (node1 == NULL)
-	{
-		printf("Failed\n");
 		return (NULL);
-	}
-	else
-	{
 		node1->n = n;
 		node1->next = *head;
+
 		*head = node1;
 	}
 	return (*head);
