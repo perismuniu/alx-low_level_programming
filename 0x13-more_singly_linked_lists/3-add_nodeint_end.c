f06702977aec961a0af2e503a39b37da43fb9a58
perismuniu <peris.muniu@gmail.com>
2023-04-04 03:44:06 -0700

    Ammendment - function that adds a new node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,6 +18,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		printf("Failed\n");
 		return (NULL);
 	}
+	if (*head == NULL)
+	{
+		*head = lastnode;
+		return (lastnode);
+	}
 	else
 	{
 		lastnode->n = n;
