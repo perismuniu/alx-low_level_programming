dd00b4de57ea47aca37f5d058875b6fdd9aa8afb
perismuniu <peris.muniu@gmail.com>
2023-04-04 03:58:52 -0700

    function that adds a new node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,12 +14,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	lastnode = (listint_t *)malloc(sizeof(listint_t));
 
 	if (lastnode == NULL)
-	{
-		printf("Failed\n");
 		return (NULL);
 		lastnode->n = n;
 		lastnode->next = NULL;
-	}
+		temp = *head;
+
 	if (*head == NULL)
 	{
 		*head = lastnode;
@@ -27,7 +26,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	else
 	{
-		temp = *head;
 		while (temp->next !=  NULL)
 		{
 		temp = temp->next;
