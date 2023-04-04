9fd30f663dc96d8ac6f29bb551b563d67f44b5ce
perismuniu <peris.muniu@gmail.com>
2023-04-04 14:52:18 -0700

    ammendment - Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
+
+	temp = head;
 	while (head != NULL)
 	{
-		temp = head;
-		free(head);
-		head = temp-> next;
+		head = temp->next;
+		free(temp);
 	}
 }
 
