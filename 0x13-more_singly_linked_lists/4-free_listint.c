39eca2b54b30ff00d369e009c7e76d0f4a67f8c3
perismuniu <peris.muniu@gmail.com>
2023-04-04 05:14:53 -0700

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *current;
 
+	current = head;
+
 	while (head != NULL)
 	{
-		current = head;
-		free(head);
 		head = current->next;
+		free(head);
 	}
 }
 
