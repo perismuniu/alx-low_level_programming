31324f026c611698545972257247043e55cca28e
perismuniu <peris.muniu@gmail.com>
2023-04-07 11:46:30 -0700

    function that sets the value of a bit at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,11 +9,16 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	*n |= ~(1UL << index);
+	mask = ~(1ul << index);
+
+	*n = (*n & mask);
+
 	return (1);
 }
