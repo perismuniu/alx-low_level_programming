fd9a2b16a6582ef4d882169aa0062e3cd6dbf4d8
perismuniu <peris.muniu@gmail.com>
2023-04-07 12:54:32 -0700

    function that returns the number of bits you would need to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits you would need to flip.
+ * @n: first number.
+ * @m: second number.
+ * Return: Number of bits (k).
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int k = 0;
+	unsigned int xor = n ^ m;
+
+	while (xor)
+	{
+		k += xor & 1;
+		xor >>= 1;
+	}
+
+	return (k);
+}
