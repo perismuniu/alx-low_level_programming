690750235b011c175ed9481cc0626d1268e86ccb
perismuniu <peris.muniu@gmail.com>
2023-04-10 12:57:35 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * append_text_to_file - function that appends text at the end of a file.
+ * create_file - function that creates a file.
  * @filename: name of file.
  * @text_content: text to append at the end of the file.
  * Return: 1 if the file exists and -1 if the file does not exist.
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int file_d;
 	int words;
