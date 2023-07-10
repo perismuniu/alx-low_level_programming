660719fb890130a315e844c232cfb1c683dddf8b
perismuniu <peris.muniu@gmail.com>
2023-07-10 13:55:31 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
