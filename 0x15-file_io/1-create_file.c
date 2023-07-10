1f4766e39f1fe5ecff6b74a5ca10129996aa3f60
perismuniu <peris.muniu@gmail.com>
2023-07-10 14:14:22 -0700

    amendment

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,36 +10,31 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file_d;
-	int words;
-	int wr;
+	ssize_t bytes_written;
 
-	if (!filename)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file_d == -1)
 	{
 		return (-1);
 	}
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
-		text_content = "";
-	}
-
-	for (words = 0; text_content[words]; words++)
-
-	wr = write(file_d, text_content, words);
-
-	if (wr == -1)
-	{
-		return (-1);
+		bytes_written = write(file_d, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(file_d);
+			return (-1);
+		}
 	}
 
 	close(file_d);
-
 	return (1);
 }
+
