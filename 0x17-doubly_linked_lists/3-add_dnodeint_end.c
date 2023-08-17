b37ba6b5136be7d724b52b93965995155abb89c3
perismuniu <peris.muniu@gmail.com>
2023-08-17 07:21:22 -0700

    function that adds a new node at the end of a doubly linked list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the end
+ * of a doubly linked list.
+ * @head: A pointer to a pointer to the head node of the list.
+ * @n: The integer value to be stored in the new node.
+ *
+ * Return: A pointer to the newly added node, or NULL on failure.
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *current;
+
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+
+		current->next = new_node;
+		new_node->prev = current;
+	}
+	return (new_node);
+}
+
+/**
+ * free_dlistint - Frees all the nodes of a doubly linked list.
+ * @head: A pointer to the head node of the list.
+ *
+ * Description: This function deallocates memory occupied by each node
+ * in the doubly linked list and sets the 'head' to NULL.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		dlistint_t *temp = head;
+
+		head = head->next;
+		free(temp);
+	}
+}
