026a4f24d4080588eebc047170a8cc235d15186d
perismuniu <peris.muniu@gmail.com>
2023-09-07 12:16:57 -0700

    function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
+	if (size == 0)
+	{	return (NULL); }
+
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{	return (NULL); }
+
 	table->size = size;
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 
@@ -19,7 +26,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		table->array[i] = NULL;
 	}
-
 	return (table);
 }
-
