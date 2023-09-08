74efe72ce72a0c0efdc673bd90b68014b047ac67
perismuniu <peris.muniu@gmail.com>
2023-09-08 13:26:10 -0700

    function that deletes a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,13 +10,18 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *current, *temp;
 	unsigned long int i;
 
+	if (!ht)
+		return;
+
 	for (i = 0; i < ht->size; i++)
 	{
 		current = ht->array[i];
-		while (current != NULL)
+		while (current)
 		{
 			temp = current;
 			current = current->next;
+			free(temp->key);
+			free(temp->value);
 			free(temp);
 		}
 	}
